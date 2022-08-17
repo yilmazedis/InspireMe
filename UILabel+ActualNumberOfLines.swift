f75f9c67c643d54deac0d70c39f5f6582de22b8e
Yılmaz Edis <ylmazedis@gmail.com>
2022-08-17 14:43:20 +0300

    Create UILabel+ActualNumberOfLines.swift

---
+++ UILabel+ActualNumberOfLines.swift
@@ -0,0 +1,21 @@
+extension UILabel {
+    var actualNumberOfLines: Int {
+        let textStorage = NSTextStorage(attributedString: self.attributedText!)
+        let layoutManager = NSLayoutManager()
+        textStorage.addLayoutManager(layoutManager)
+        let textContainer = NSTextContainer(size: self.bounds.size)
+        textContainer.lineFragmentPadding = 0
+        textContainer.lineBreakMode = self.lineBreakMode
+        layoutManager.addTextContainer(textContainer)
+
+        let numberOfGlyphs = layoutManager.numberOfGlyphs
+        var numberOfLines = 0, index = 0, lineRange = NSMakeRange(0, 1)
+
+        while index < numberOfGlyphs {
+            layoutManager.lineFragmentRect(forGlyphAt: index, effectiveRange: &lineRange)
+            index = NSMaxRange(lineRange)
+            numberOfLines += 1
+        }
+        return numberOfLines
+    }
+}

63c04923135f7f0a923a98d1963e4236da6371f4
Yılmaz Edis <ylmazedis@gmail.com>
2022-09-22 00:04:59 +0300

    Added Point

--- AddGradient.swift
+++ AddGradient.swift
@@ -9,11 +9,46 @@
     
    func addGradient(to view: UIView) {
         let gradientLayer = CAGradientLayer()
+        // gradientLayer.type = .axial
         gradientLayer.frame = view.layer.bounds;
-        gradientLayer.colors = [AppColor.premiumThirdGradient.cgColor,
-                                AppColor.premiumSecondGradient.cgColor,
-                                AppColor.premiumFirstGradient.cgColor]
+        gradientLayer.colors = [UIColor.red.cgColor, UIColor.green.cgColor, UIColor.blue.cgColor]
         gradientLayer.startPoint = CGPoint(x: 1.0, y: 0.5)
         gradientLayer.endPoint = CGPoint(x: 0.0, y: 0.5)
+        // gradientLayer.transform = CATransform3DMakeRotation(270 / 180 * CGFloat.pi, 0, 0, 1) // New line
+        // gradientLayer.locations = [0.0, 0.3, 0.7] // New line
         view.layer.insertSublayer(gradientLayer, at: 0);
+    }
+
+    enum Point {
+        case topLeft
+        case centerLeft
+        case bottomLeft
+        case topCenter
+        case center
+        case bottomCenter
+        case topRight
+        case centerRight
+        case bottomRight
+        var point: CGPoint {
+            switch self {
+            case .topLeft:
+                return CGPoint(x: 0, y: 0)
+            case .centerLeft:
+                return CGPoint(x: 0, y: 0.5)
+            case .bottomLeft:
+                return CGPoint(x: 0, y: 1.0)
+            case .topCenter:
+                return CGPoint(x: 0.5, y: 0)
+            case .center:
+                return CGPoint(x: 0.5, y: 0.5)
+            case .bottomCenter:
+                return CGPoint(x: 0.5, y: 1.0)
+            case .topRight:
+                return CGPoint(x: 1.0, y: 0.0)
+            case .centerRight:
+                return CGPoint(x: 1.0, y: 0.5)
+            case .bottomRight:
+                return CGPoint(x: 1.0, y: 1.0)
+            }
+        }
     }

cdca091ffb4162074649fd6169cf0c0513b21d63
Yılmaz Edis <ylmazedis@gmail.com>
2023-02-15 14:47:43 +0300

    Create SwipeToDeleteView.swift

---
+++ Gestures/SwipeToDeleteView.swift
@@ -0,0 +1,39 @@
+class SwipeToDeleteView: UIView {
+    
+    var deleteHandler: (() -> Void)?
+    private let panGestureRecognizer = UIPanGestureRecognizer()
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        setupGestureRecognizers()
+    }
+    
+    required init?(coder: NSCoder) {
+        super.init(coder: coder)
+        setupGestureRecognizers()
+    }
+    
+    private func setupGestureRecognizers() {
+        panGestureRecognizer.addTarget(self, action: #selector(handlePanGesture(_:)))
+        addGestureRecognizer(panGestureRecognizer)
+    }
+    
+    @objc private func handlePanGesture(_ gestureRecognizer: UIPanGestureRecognizer) {
+        let translation = gestureRecognizer.translation(in: self)
+        
+        switch gestureRecognizer.state {
+        case .changed:
+            transform = CGAffineTransform(translationX: translation.x, y: 0)
+        case .ended:
+            if translation.x < -bounds.size.width / 2 {
+                deleteHandler?()
+            } else {
+                UIView.animate(withDuration: 0.2) {
+                    self.transform = .identity
+                }
+            }
+        default:
+            break
+        }
+    }
+}

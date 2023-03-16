3b40d0a10aa02210703bee8fc540467cc752e128
Yılmaz Edis <ylmazedis@gmail.com>
2023-03-16 09:08:11 +0300

    Update SetUITextFieldWritableKeyPath.swift

--- BasicAlgorithms/EfficientWay/SetUITextFieldWritableKeyPath.swift
+++ BasicAlgorithms/EfficientWay/SetUITextFieldWritableKeyPath.swift
@@ -1,5 +1,4 @@
 class ViewController: UIViewController {
-    
     @IBOutlet weak var cityTextField: UITextField!
     @IBOutlet weak var countyTextField: UITextField!
     @IBOutlet weak var municialityTextField: UITextField!
@@ -7,132 +6,44 @@ class ViewController: UIViewController {
     @IBOutlet weak var streetTextField: UITextField!
     @IBOutlet weak var buildingNoTextField: UITextField!
     @IBOutlet weak var apartmentNoTextField: UITextField!
-
-    // The rest of your code goes here...
-    func initCodesBestWay() {
-        var address = DaskPropasalModel()
-        
-        // if you use KeyPath instead of WritableKeyPath, you cannot use address[keyPath: keyPath], beacue it will be read-only otherwise.
-        let fields: [UITextField: WritableKeyPath<DaskPropasalModel, TextValueItemModel?>] = [
-            cityTextField: \.city,
-            countyTextField: \.county,
-            municialityTextField: \.municipality,
-            districtTextField: \.district,
-            streetTextField: \.avenueStreet,
-            buildingNoTextField: \.buildingNumber,
-            apartmentNoTextField: \.apartmantNumber
-        ]
-
+    
+    private lazy var fields: [UITextField: WritableKeyPath<ViewModel, String?>] = [
+        cityTextField: \ViewModel.city,
+        countyTextField: \.county,
+        municialityTextField: \.municipality,
+        districtTextField: \.district,
+        streetTextField: \.avenueStreet,
+        buildingNoTextField: \.buildingNumber,
+        apartmentNoTextField: \.apartmantNumber
+    ]
+    
+    func setTextFieldsBestWay() -> ViewModel {
+        var address = ViewModel()
         for (field, keyPath) in fields {
-            let textValueItem = TextValueItemModel()
-            textValueItem.text = field.text ?? ""
-            address[keyPath: keyPath] = textValueItem
+            address[keyPath: keyPath] = field.text ?? ""
         }
+        return address
     }
     
-    func initTwo() {
-      let address = DaskPropasalModel()
-
-      let fields = [self.cityTextField, self.countyTextField, self.municialityTextField, self.districtTextField, self.streetTextField, self.buildingNoTextField, self.apartmentNoTextField]
-
-      for (index, field) in fields.enumerated() {
-          let textValueItem = TextValueItemModel()
-          textValueItem.text = field?.text ?? ""
-
-          switch index {
-          case fields.firstIndex(of: self.cityTextField):
-              address.city = textValueItem
-          case fields.firstIndex(of: self.countyTextField):
-              address.county = textValueItem
-          case fields.firstIndex(of: self.municialityTextField):
-              address.municipality = textValueItem
-          case fields.firstIndex(of: self.districtTextField):
-              address.district = textValueItem
-          case fields.firstIndex(of: self.streetTextField):
-              address.avenueStreet = textValueItem
-          case fields.firstIndex(of: self.buildingNoTextField):
-              address.buildingNumber = textValueItem
-          case fields.firstIndex(of: self.apartmentNoTextField):
-              address.apartmantNumber = textValueItem
-          default:
-              break
-          }
-      }
-    }
-    
-    func initOne() {
-        let address = DaskPropasalModel()
-
-        let fields = [self.cityTextField, self.countyTextField, self.municialityTextField, self.districtTextField, self.streetTextField, self.buildingNoTextField, self.apartmentNoTextField]
-
-        for (index, field) in fields.enumerated() {
-            let textValueItem = TextValueItemModel()
-            textValueItem.text = field?.text ?? ""
-            
-            switch index {
-            case 0:
-                address.city = textValueItem
-            case 1:
-                address.county = textValueItem
-            case 2:
-                address.municipality = textValueItem
-            case 3:
-                address.district = textValueItem
-            case 4:
-                address.avenueStreet = textValueItem
-            case 5:
-                address.buildingNumber = textValueItem
-            case 6:
-                address.apartmantNumber = textValueItem
-            default:
-                break
-            }
-        }
-    }
-    
-    func initBadWay() {
-      let address = DaskPropasalModel()
-        
-        let city = TextValueItemModel()
-        city.text = self.cityTextField.text ?? ""
-        address.city = city
-        
-        let county = TextValueItemModel()
-        county.text = self.countyTextField.text ?? ""
-        address.county = county
-        
-        let municipality = TextValueItemModel()
-        municipality.text = self.municialityTextField.text ?? ""
-        address.municipality = municipality
-        
-        let district = TextValueItemModel()
-        district.text = self.districtTextField.text ?? ""
-        address.district = district
-        
-        let avenueStreet = TextValueItemModel()
-        avenueStreet.text = self.streetTextField.text ?? ""
-        address.avenueStreet = avenueStreet
-        
-        let buildingNumber = TextValueItemModel()
-        buildingNumber.text = self.buildingNoTextField.text ?? ""
-        address.buildingNumber = buildingNumber
-        
-        let apartmantNumber = TextValueItemModel()
-        apartmantNumber.text = self.apartmentNoTextField.text ?? ""
-        address.apartmantNumber = apartmantNumber
+    func setTextFieldsBadWay() -> ViewModel {
+        let address = ViewModel()
+        address.city = self.cityTextField.text ?? ""
+        address.county = self.countyTextField.text ?? ""
+        address.municipality =  self.municialityTextField.text ?? ""
+        address.district = self.districtTextField.text ?? ""
+        address.avenueStreet = self.streetTextField.text ?? ""
+        address.buildingNumber = self.buildingNoTextField.text ?? ""
+        address.apartmantNumber = self.apartmentNoTextField.text ?? ""
+        return address
     }
 }
 
-class DaskPropasalModel {
-    var city: TextValueItemModel?
-    var county: TextValueItemModel?
-    var municipality: TextValueItemModel?
-    var district: TextValueItemModel?
-    var avenueStreet: TextValueItemModel?
-    var buildingNumber: TextValueItemModel?
-    var apartmantNumber: TextValueItemModel?
-}
-
-class TextValueItemModel: UILabel {
-    let city: String = ""
+class ViewModel {
+    var city: String?
+    var county: String?
+    var municipality: String?
+    var district: String?
+    var avenueStreet: String?
+    var buildingNumber: String?
+    var apartmantNumber: String?
 }

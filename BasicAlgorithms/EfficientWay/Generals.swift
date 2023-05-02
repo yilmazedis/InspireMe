e6d53ec76234524c288b7b72ce33f22cf233c69b
Yılmaz Edis <ylmazedis@gmail.com>
2023-05-02 19:38:27 +0300

    Update Generals.swift

--- BasicAlgorithms/EfficientWay/Generals.swift
+++ BasicAlgorithms/EfficientWay/Generals.swift
@@ -5,7 +5,17 @@ analyzeButton.isHidden = selectedItems.count > 1 ? false : true
 analyzeButton.isHidden = selectedItems.count <= 1
 
 /// ---------------------
-
 func copyArrayExceptFirst<T>(_ array: [T]) -> [T] {
     return Array(array.dropFirst())
+}
+
+/// ---------------------
+// Bad Case
+var getAnyValidRemoteUrl: URL? {
+    return previewUrl ?? thumnailUrl ?? downloadUrl ?? smalURl ?? mediumUrl ?? largeUrl 
+}
+
+// Better Case
+var getAnyValidRemoteUrl: URL? {
+    return [previewUrl, thumnailUrl, downloadUrl, smalURl, mediumUrl, largeUrl].compactMap({$0}).first
 }

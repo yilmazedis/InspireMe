eb6e8a91b647aa2b59cc249a8553cef4239483de
Yılmaz Edis <ylmazedis@gmail.com>
2023-05-06 13:15:00 +0300

    Create ToggleButton.swift

---
+++ UIButton/ToggleButton.swift
@@ -0,0 +1,23 @@
+private lazy var showButton: UIButton = {
+    let newValue = UIButton(type: .custom)
+    newValue.titleLabel?.font = .appFont(.medium, size: 14)
+
+    // normal
+    newValue.setTitle(TextConstants.showMore, for: .normal)
+    newValue.setTitleColor(ColorConstants.textGrayColor, for: .normal)
+    newValue.setImage(Image.iconArrowDownSmall.image, for: .normal)
+
+    // selected
+    newValue.setTitle(TextConstants.showLess, for: .selected)
+    newValue.setTitleColor(ColorConstants.textGrayColor, for: .selected)
+    newValue.setImage(Image.iconArrowUpSmall.image, for: .selected)
+
+    newValue.forceImageToRightSide()
+
+    newValue.addTarget(self, action: #selector(onCollapseTap), for: .touchUpInside)
+    return newValue
+}()
+
+@IBAction func toggleButtonAction(_ sender: UIButton) {
+    sender.isSelected.toggle()
+}

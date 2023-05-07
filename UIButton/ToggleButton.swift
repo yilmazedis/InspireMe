647fce2fa47a564fff26de9e33cb03c0135b0be6
Yılmaz Edis <ylmazedis@gmail.com>
2023-05-07 16:19:54 +0300

    Update ToggleButton.swift

--- UIButton/ToggleButton.swift
+++ UIButton/ToggleButton.swift
@@ -21,3 +21,18 @@ private lazy var showButton: UIButton = {
 @IBAction func toggleButtonAction(_ sender: UIButton) {
     sender.isSelected.toggle()
 }
+
+
+extension UIButton {
+    /// https://stackoverflow.com/a/43785519/5893286
+    func forceImageToRightSide() {
+        /// or 1
+        let newTransform = CGAffineTransform(scaleX: -1.0, y: 1.0) 
+        transform = newTransform
+        titleLabel?.transform = newTransform
+        imageView?.transform = newTransform
+        
+        /// or 2
+        //sortByButton.semanticContentAttribute = .forceRightToLeft
+    }
+}

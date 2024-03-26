f1d6824640564eb62678f516ea6f6be403d586fc
Yılmaz Edis <ylmazedis@gmail.com>
2024-03-26 16:25:36 +0300

    Update Some.swift

--- Generics/Some.swift
+++ Generics/Some.swift
@@ -6,6 +6,8 @@ protocol Car {
 struct BMW: Car {
     //typealias Identifier = String
     let id: String
+    // If you pass BMW as paramater in Car type. You cannot access name.
+    let name: String = "BMW"
     
     init(id: String) {
         self.id = id
@@ -21,9 +23,12 @@ struct Honda: Car {
     }
 }
 
-func getFavoriteCar() -> some Car {
+// any or some
+func getFavoriteCar(car: any Car) -> some Car {
+    let id = car.id
+    // let name = car.name // you cannot access name, because it comes from Car protocol
     return BMW(id: "bmw")
     //return Honda(id: 123)
 }
 
-print(String(describing: getFavoriteCar()))
+print(String(describing: getFavoriteCar(car: BMW(id: "12314"))))
